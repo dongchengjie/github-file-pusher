cad0da61b73e5ff05bea9dd125faf218c87f986f
dongchengjie <2424014450@qq.com>
2024-02-22 03:37:50 +0800

    update desc

--- Src/Program.cs
+++ Src/Program.cs
@@ -26,14 +26,14 @@ namespace gfp.Src
             {
                 Console.WriteLine(HelpText.AutoBuild(result, settings =>
                 {
-                    settings.AddDashesToOption = true;              // 显示破折号
-                    settings.AddEnumValuesToHelpText = true;        // 描述枚举值的取值范围
-                    settings.AdditionalNewLineAfterOption = false;  // 描述末尾空新行
-                    settings.AutoHelp = true;                       // 自动生成--help
-                    settings.AutoVersion = true;                    // 自动生成--version
-                    settings.Copyright = string.Empty;              // 版权信息
-                    settings.Heading = "Github file pusher";        // 顶部信息
-                    settings.MaximumDisplayWidth = int.MaxValue;    // 单行展示字数限制
+                    settings.AddDashesToOption = true;                                          // 显示破折号
+                    settings.AddEnumValuesToHelpText = true;                                    // 描述枚举值的取值范围
+                    settings.AdditionalNewLineAfterOption = false;                              // 描述末尾空新行
+                    settings.AutoHelp = true;                                                   // 自动生成--help
+                    settings.AutoVersion = true;                                                // 自动生成--version
+                    settings.Copyright = string.Empty;                                          // 版权信息
+                    settings.Heading = "A CLI program for updating Github files over http.";    // 顶部信息
+                    settings.MaximumDisplayWidth = int.MaxValue;                                // 单行展示字数限制
                     return HelpText.DefaultParsingErrorsHandler(result, settings);
                 }, e => e));
             }
